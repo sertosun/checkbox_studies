4942dcfbde6e14c1c1aba93afb4c6464795a2898
Sadeddin Ertosun <82061290+sertosun@users.noreply.github.com>
2022-05-12 08:47:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function myFunction() {
   let colorChg = document.getElementById("colorChg");
-  let leon = document.getElementById("leon");
+  let lion = document.getElementById("lion");
   let eagle = document.getElementById("eagle");
 
   if (colorChg.checked) {
     document.body.style.backgroundColor = "black";
-    leon.style.display = "none";
+    lion.style.display = "none";
     eagle.style.display = "block";
   } else {
     document.body.style.backgroundColor = "yellow";
     eagle.style.display = "none";
-    leon.style.display = "block";
+    lion.style.display = "block";
   }
 }
